1377252b7c1b26b9b31395e7d4f94615d8338537
tourniquet <admyn3d@gmail.com>
2023-06-29 10:49:19 +0100

    Added NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import { Col, Row } from 'antd'
 
 import { TodoProvider } from '../contexts/TodoContext'
 
-import NewTodo from '../components/NewTodo/NewTodo'
-import UnorderedList from '../components/UnorderedList/UnorderedList'
+import NavBar from './components/NavBar/NavBar'
+import NewTodo from './components/NewTodo/NewTodo'
+import UnorderedList from './components/UnorderedList/UnorderedList'
 
 export default function Home (): JSX.Element {
   return (
     <TodoProvider>
+      <NavBar />
+
       <Row>
         <Col span={12} offset={6}>
           <NewTodo />
