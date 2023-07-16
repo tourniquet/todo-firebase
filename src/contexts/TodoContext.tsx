7cb1c81417063cbf5acd39d9decb6f11d5a1162e
tourniquet <admyn3d@gmail.com>
2023-07-16 12:46:44 +0100

    user can authenticate now , fetching only user's todos

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,23 +1,40 @@
 'use client'
 
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where, orderBy } from 'firebase/firestore'
 import { createContext, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
-import { db } from '../../firebase-config'
+import { auth, db } from '../../firebase-config'
 
 export const TodoContext = createContext(null)
 
 export function TodoProvider ({ children }): JSX.Element {
   const [todos, setTodos] = useState([])
+  const [user, loading] = useAuthState(auth)
   const todosCollectionRef = collection(db, 'todos')
 
-  const getTodos = async () => {
-    const data = await getDocs(todosCollectionRef)
-    setTodos(data.docs.map((todo) => ({ ...todo.data(), id: todo.id })))
+  const getTodos = async (uid) => {
+    const q = query(
+      todosCollectionRef,
+      where('uid', '==', uid),
+      orderBy('createdAt')
+    )
+    const data = await getDocs(q)
+
+    setTodos(data.docs.map((todos) => {
+      return { ...todos.data(), id: todos.id }
+    }))
   }
 
   return (
-    <TodoContext.Provider value={{ todos, todosCollectionRef, setTodos, getTodos }}>
+    <TodoContext.Provider value={{
+      user,
+      todos,
+      todosCollectionRef,
+      setTodos,
+      getTodos
+    }}
+    >
       {children}
     </TodoContext.Provider>
   )
