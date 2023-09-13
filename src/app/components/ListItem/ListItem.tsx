abd199f06f07c602135dd4338350f55e23a2a93c
tourniquet <admyn3d@gmail.com>
2023-09-14 00:23:23 +0100

    getting rid of ant design

---
+++ src/app/components/ListItem/ListItem.tsx
@@ -0,0 +1,70 @@
+import { CloseOutlined, EditOutlined } from '@ant-design/icons'
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore'
+import { useContext } from 'react'
+import styled from 'styled-components'
+
+import { TodoContext } from '@/contexts/TodoContext'
+import { db } from '../../../../firebase-config'
+
+const LiStyled = styled.li`
+  align-items: center;
+  border-block-end: 1px solid rgba(5, 5, 5, 0.06);
+  display: flex;
+  justify-content: space-between;
+  padding-inline: 24px;
+  padding: 12px 0;
+`
+
+const LabelStyled = styled.label`
+  align-items: center;
+  display: inline-flex;
+`
+
+const SpanStyled = styled.span`
+  padding-inline-end: 8px;
+  padding-inline-start: 8px;
+`
+
+interface TodoProps {
+  id: string
+  done: boolean
+  todo: string
+}
+
+function ListItem ({ todo }: { todo: TodoProps }): JSX.Element {
+  const { todos, setTodos }: { todos?: any, setTodos?: any } = useContext(TodoContext)
+
+  const doneTodo = async (id: string): Promise<void> => {
+    const todo = doc(db, 'todos', id)
+    await updateDoc(todo, { done: true })
+    setTodos(todos.map((todo: { id: string }) => (todo.id === id) ? { ...todo, done: true } : todo))
+  }
+
+  const deleteTodo = async (id: string): Promise<void> => {
+    const itemDoc = doc(db, 'todos', id)
+    await deleteDoc(itemDoc)
+
+    setTodos(todos.filter((todo: { id: string }) => todo.id !== id))
+  }
+
+  return (
+    <LiStyled>
+      <LabelStyled>
+        <input
+          type='checkbox'
+          checked={todo.done}
+          disabled={todo.done}
+          onChange={() => { void doneTodo(todo.id) }}
+        />
+        <SpanStyled>
+          {todo.todo}
+        </SpanStyled>
+      </LabelStyled>
+
+      {!todo.done && (<EditOutlined />)}
+      <CloseOutlined onClick={() => { void deleteTodo(todo.id) }} />
+    </LiStyled>
+  )
+}
+
+export default ListItem
